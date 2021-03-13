e9094f1ebb390ee71076354135d970ff84685c32
fcsouza <fcsouza18@hotmail.com>
2021-03-12 21:11:56 -0300

    cinlude user and survey controller

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,13 @@
 import { Router } from "express";
 
+import { SurveysController } from "./controllers/SurveysController";
+import { UserController } from "./controllers/UserController";
+
+
 const router = Router();
+
+const userController = new UserController();
+const surveysController = new SurveysController();
+
 
 export { router };
