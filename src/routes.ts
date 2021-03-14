f1cec568dc46a763c25de7e80156b0f02e9da7e1
fcsouza <fcsouza18@hotmail.com>
2021-03-14 19:44:22 -0300

    include routes

--- src/routes.ts
+++ src/routes.ts
@@ -2,12 +2,25 @@ import { Router } from "express";
 
 import { SurveysController } from "./controllers/SurveysController";
 import { UserController } from "./controllers/UserController";
-
+import { SendMailController } from "./controllers/SendMailController";
+import { AnswerController } from "./controllers/AnswerController";
 
 const router = Router();
 
 const userController = new UserController();
 const surveysController = new SurveysController();
 
+const sendMailController = new SendMailController();
+
+const answerController = new AnswerController();
+
+router.post("/users", userController.create);
+
+router.post("/surveys", surveysController.create);
+router.get("/surveys", surveysController.show);
+
+router.post("/sendMail", sendMailController.execute);
+
+router.get("/answers/:value", answerController.execute);
 
 export { router };
